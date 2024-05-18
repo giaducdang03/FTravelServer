4d85c11759d8aca0e3d71a2e2363fe4b109e3884
Dang Gia Duc <giaducdang@gmail.com>
2024-05-19 00:44:17 +0700

    initial project

---
+++ FTravel.API/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using FTravel.API.Middlewares;
+using System.Diagnostics;
+
+namespace FTravel.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddWebAPIService(this IServiceCollection services)
+        {
+            // use DI here
+
+
+            services.AddHealthChecks();
+            services.AddSingleton<GlobalExceptionMiddleware>();
+            services.AddSingleton<PerformanceMiddleware>();
+            services.AddSingleton<Stopwatch>();
+            services.AddHttpContextAccessor();
+
+            return services;
+        }
+    }
+}
---
+++ FTravel.API/Program.cs
@@ -0,0 +1,66 @@
+using FTravel.API;
+using FTravel.API.Middlewares;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("app-cors",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .WithExposedHeaders("X-Pagination")
+            .AllowAnyMethod();
+        });
+});
+
+builder.Services.AddWebAPIService();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FTravel Web API", Version = "v.1.0" });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FTravel API v.01");
+});
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+app.UseMiddleware<PerformanceMiddleware>();
+
+app.UseHttpsRedirection();
+
+app.UseCors("app-cors");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
