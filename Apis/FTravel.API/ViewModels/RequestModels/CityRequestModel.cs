87d7a422932caa1b52f12d19390370972a13d511
SE173453-PhuocTan <phuoctan0206@gmail.com>
2024-06-02 21:28:17 +0700

    Update CityRequestModel.cs

--- Apis/FTravel.API/ViewModels/RequestModels/CityRequestModel.cs
+++ Apis/FTravel.API/ViewModels/RequestModels/CityRequestModel.cs
@@ -1,8 +1,12 @@
-﻿namespace FTravel.API.ViewModels.RequestModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FTravel.API.ViewModels.RequestModels
 {
     public class CityRequestModel
     {
+        [MaxLength(100)]
         public string? UnsignName { get; set; }
+        [MaxLength(100)]
         public string Name { get; set; } = null!;
         public DateTime CreateDate { get; set; }
 
