42aa41bb2f99688ca379bd8fa28a4a9f77d9b4de
Nguyen Duc Anh <anhndse173319@fpt.edu.vn>
2024-06-07 23:22:42 +0700

    fix feat

--- Apis/FTravel.Service/Mapper/MapperConfigProfile.cs
+++ Apis/FTravel.Service/Mapper/MapperConfigProfile.cs
@@ -21,10 +21,11 @@ namespace FTravel.Service.Mapper
             CreateMap<Wallet, WalletModel>();
             CreateMap<Transaction, TransactionModel>();
             CreateMap<Route, RouteModel>();
-            CreateMap<City, CityModel>().ReverseMap();
-            CreateMap<TicketType, TicketTypeModel>();
             CreateMap<StationModel, Station>().ReverseMap();
 
+
+            CreateMap<City, CityModel>().ReverseMap();
+            CreateMap<TicketType, TicketTypeModel>();
 
             CreateMap<Repository.EntityModels.Service, ServiceModel>()
             .ForMember(dest => dest.RouteName, opt => opt.MapFrom(src => src.Route != null ? src.Route.Name : string.Empty))
