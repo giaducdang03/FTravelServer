20479539846a9499676b34be98f58c368aba300d
Dang Gia Duc <giaducdang@gmail.com>
2024-06-12 11:03:29 +0700

    update: add firebase noti lib, fix table Notification

---
+++ Apis/FTravel.Repository/Repositories/NotificationRepository.cs
@@ -0,0 +1,18 @@
+﻿using FTravel.Repository.DBContext;
+using FTravel.Repository.EntityModels;
+using FTravel.Repository.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FTravel.Repository.Repositories
+{
+    public class NotificationRepository : GenericRepository<Notification>, INotificationRepository
+    {
+        public NotificationRepository(FtravelContext context) : base(context)
+        {
+        }
+    }
+}
