8705d9acb33d89560cecee13f7399d4502261001
nhanntse173258 <nhanntse173258@fpt.edu.vn>
2024-07-13 09:43:48 +0700

    fix: add create and update date when get tríp

--- Apis/FTravel.Service/BusinessModels/TripModels/TripModel.cs
+++ Apis/FTravel.Service/BusinessModels/TripModels/TripModel.cs
@@ -26,6 +26,8 @@ namespace FTravel.Service.BusinessModels.TripModels
         public string? Status { get; set; }
         public bool? IsTemplate { get; set; }
         public int? DriverId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
         public List<TicketModel> Tickets { get; set; }
         public List<TripServiceModel> Services { get; set; }
     }
